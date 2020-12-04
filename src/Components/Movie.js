33aa160c17ad637b2d8ff74b06ae6d97ea9f482e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-12-04 09:55:02 -0600

    added modal to movie info

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import RateMovie from './RateMovie';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ModalFooter from 'react-bootstrap/ModalFooter';
 
 
-class Movie extends React.Component {
+export default class Movie extends React.Component {
+    state = {
+      showMovie: false
+    }
+
+  handleShowMovie = () => {
+    this.setState({
+      showMovie: true
+    })
+  }
+
+  handleCloseShowMovie = () => {
+    this.setState({
+      showMovie: false
+    })
+  }
+
    render() {
        const {Title, Poster, Year, imdbID, db_id, thumbs_up, thumbs_down} = this.props.movie;
 
@@ -29,9 +49,39 @@ class Movie extends React.Component {
                 ) : null }
                </div>
                <div>
-               <div style={{paddingBottom: "2%"}}>
+               <Button 
+                 className="btn-lg" 
+                 style={{ 
+                         backgroundColor: "#242424", 
+                         color: "#f2f2f2", 
+                         width: "25%",
+                         fontSize: "30px",
+                         marginTop: "5%",
+                         border: "3px solid #fafafa"
+                         }}
+                 type="submit"
+                 onClick={this.handleShowMovie}>More Info
+               </Button>
+               <Modal show={this.state.showMovie}>
+               <ModalBody>
+                <div style={{paddingBottom: "2%"}}>
                    <h2 className="year">{Year}</h2>
                </div>
+               </ModalBody>
+                 <ModalFooter>
+                   <Button  
+                     style={{ 
+                       backgroundColor: "#242424", 
+                       color: "#f2f2f2",
+                       width: "100%",
+                       fontSize: "30px",
+                       border: "3px solid #fafafa",
+                       fontFamily: "Trebuchet MS, Helvetica, sans-serif"
+                     }} 
+                     onClick={this.handleCloseShowMovie}>Close
+                   </Button>
+                 </ModalFooter>
+               </Modal>
                <RateMovie thumbs_up={thumbs_up} thumbs_down={thumbs_down} title={Title} db_id={db_id} imdb_number={imdbID}/>
                </div>
             </Card>
@@ -39,5 +89,3 @@ class Movie extends React.Component {
        )
    }
 }
-
-export default Movie;
--- src/Components/RateMovie.js
+++ src/Components/RateMovie.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import { addFilm, updateFilm } from '../Services/RailsDB.js';
 
 // import ReactDOM from 'react-dom'
